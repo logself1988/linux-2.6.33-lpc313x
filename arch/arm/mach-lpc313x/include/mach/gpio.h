f19d7b437d3012b5a87d6c6ae2457fa02aaf5588
Ingo Albrecht <prom@berlin.ccc.de>
2011-01-09 04:58:25 +0100

    gpio: fix indentation.

--- arch/arm/mach-lpc313x/include/mach/gpio.h
+++ arch/arm/mach-lpc313x/include/mach/gpio.h
@@ -60,7 +60,7 @@
 #define GPIO_MGPIO7           (IOCONF_EBI_MCI | 29) 
 #define GPIO_MGPIO8           (IOCONF_EBI_MCI | 30) 
 #define GPIO_MGPIO10          (IOCONF_EBI_MCI | 31) 
-                
+
 #define GPIO_MNAND_RYBN1      (IOCONF_EBI_I2STX_0 | 0) 
 #define GPIO_MNAND_RYBN2      (IOCONF_EBI_I2STX_0 | 1) 
 #define GPIO_MNAND_RYBN3      (IOCONF_EBI_I2STX_0 | 2) 
@@ -71,22 +71,22 @@
 #define GPIO_EBI_NRAS_BLOUT   (IOCONF_EBI_I2STX_0 | 7) 
 #define GPIO_EBI_A_0_ALE      (IOCONF_EBI_I2STX_0 | 8) 
 #define GPIO_EBI_NWE          (IOCONF_EBI_I2STX_0 | 9) 
-                 
+
 #define GPIO_CGU_SYSCLK_O     (IOCONF_CGU | 0) 
 
 #define GPIO_I2SRX_BCK0       (IOCONF_I2SRX_0 | 0) 
 #define GPIO_I2SRX_DATA0      (IOCONF_I2SRX_0 | 1) 
 #define GPIO_I2SRX_WS0        (IOCONF_I2SRX_0 | 2) 
-                  
+
 #define GPIO_I2SRX_DATA1      (IOCONF_I2SRX_1 | 0) 
 #define GPIO_I2SRX_BCK1       (IOCONF_I2SRX_1 | 1) 
 #define GPIO_I2SRX_WS1        (IOCONF_I2SRX_1 | 2) 
-                  
+
 #define GPIO_I2STX_DATA1      (IOCONF_I2STX_1 | 0) 
 #define GPIO_I2STX_BCK1       (IOCONF_I2STX_1 | 1) 
 #define GPIO_I2STX_WS1        (IOCONF_I2STX_1 | 2) 
 #define GPIO_I2STX_256FS_O    (IOCONF_I2STX_1 | 3) 
- 
+
 #define GPIO_EBI_D_9          (IOCONF_EBI | 0)  
 #define GPIO_EBI_D_10         (IOCONF_EBI | 1)  
 #define GPIO_EBI_D_11         (IOCONF_EBI | 2)  
@@ -103,7 +103,7 @@
 #define GPIO_EBI_D_7          (IOCONF_EBI | 13) 
 #define GPIO_EBI_D_8          (IOCONF_EBI | 14) 
 #define GPIO_EBI_D_15         (IOCONF_EBI | 15) 
-                  
+
 
 #define GPIO_GPIO1            (IOCONF_GPIO | 0)  
 #define GPIO_GPIO0            (IOCONF_GPIO | 1)  
@@ -120,28 +120,28 @@
 #define GPIO_GPIO18           (IOCONF_GPIO | 12) 
 #define GPIO_GPIO19           (IOCONF_GPIO | 13) 
 #define GPIO_GPIO20           (IOCONF_GPIO | 14) 
-                
+
 #define GPIO_I2C_SDA1         (IOCONF_I2C1 | 0) 
 #define GPIO_I2C_SCL1         (IOCONF_I2C1 | 1) 
-                          
+
 #define GPIO_SPI_MISO         (IOCONF_SPI | 0) 
 #define GPIO_SPI_MOSI         (IOCONF_SPI | 1) 
 #define GPIO_SPI_CS_IN        (IOCONF_SPI | 2) 
 #define GPIO_SPI_SCK          (IOCONF_SPI | 3) 
 #define GPIO_SPI_CS_OUT0      (IOCONF_SPI | 4) 
-                 
+
 #define GPIO_NAND_NCS_3       (IOCONF_NAND_CTRL | 0)
 #define GPIO_NAND_NCS_0       (IOCONF_NAND_CTRL | 1)
 #define GPIO_NAND_NCS_1       (IOCONF_NAND_CTRL | 2)
 #define GPIO_NAND_NCS_2       (IOCONF_NAND_CTRL | 3)
-                 
+
 #define GPIO_PWM_DATA         (IOCONF_PWM | 0)
-                  
+
 #define GPIO_UART_RXD         (IOCONF_UART | 0)
 #define GPIO_UART_TXD         (IOCONF_UART | 1)
-                
-                
-                
+
+
+
 static inline int lpc313x_gpio_direction_input(unsigned gpio)
 {
 	unsigned long flags;
@@ -204,40 +204,40 @@ static inline void lpc313x_gpio_set_value(unsigned gpio, int value)
 
 static inline int gpio_get_value(unsigned gpio)
 {
-    if(gpio < NUM_FAST_GPIO) {
-	unsigned bit = (1 << gpio);
-	if(GPIO_STATE(IOCONF_FAST_GPIO) & bit) {
-	    return 1;
+	if(gpio < NUM_FAST_GPIO) {
+		unsigned bit = (1 << gpio);
+		if(GPIO_STATE(IOCONF_FAST_GPIO) & bit) {
+			return 1;
+		} else {
+			return 0;
+		}
 	} else {
-	    return 0;
+		return __gpio_get_value(gpio);
 	}
-    } else {
-	return __gpio_get_value(gpio);
-    }
 }
 
 static inline void gpio_set_value(unsigned gpio, int value)
 {
-    if(gpio < NUM_FAST_GPIO) {
-	unsigned bit = (1 << gpio);
-	if(value) {
-		GPIO_M0_SET(IOCONF_FAST_GPIO) = bit;
+	if(gpio < NUM_FAST_GPIO) {
+		unsigned bit = (1 << gpio);
+		if(value) {
+			GPIO_M0_SET(IOCONF_FAST_GPIO) = bit;
+		} else {
+			GPIO_M0_RESET(IOCONF_FAST_GPIO) = bit;
+		}
 	} else {
-		GPIO_M0_RESET(IOCONF_FAST_GPIO) = bit;
+		__gpio_set_value(gpio, value);
 	}
-    } else {
-	__gpio_set_value(gpio, value);
-    }
 }
 
 static inline int gpio_cansleep(unsigned gpio)
 {
-    return __gpio_cansleep(gpio);
+	return __gpio_cansleep(gpio);
 }
 
 static inline int gpio_to_irq(unsigned gpio)
 {
-    return __gpio_to_irq(gpio);
+	return __gpio_to_irq(gpio);
 }
 
 #endif /*_LPC313X_GPIO_H*/
